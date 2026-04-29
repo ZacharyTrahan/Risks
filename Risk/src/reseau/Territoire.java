d8739b475e4bbcd29a74aca0f5b9b5e5393de0ad
2542153 <2542153@cegeplimoilou.ca>
2026-04-29 10:35:14 -0400

    ajout attribut: long serialVersionUID=1L; ajout constructeur Territoire(String nom) ajout constructeur Territoire(String nom, Etat etatBase) ajout Getter/Setter: Etat getEtat() void setEtat(etat) String getNom() void setNom(nom)

--- Risk/src/reseau/Territoire.java
+++ Risk/src/reseau/Territoire.java
@@ -1,10 +1,37 @@
 package reseau;
 
-public class Territoire {
+import java.io.Serializable;
+
+public class Territoire implements Serializable {
 
     private Etat etat;
     private String nom;
     private final static Etat ETAT_BASE = Etat.NEUTRE;
+    private static final long serialVersionUID=1L;
+
+    public Territoire(String nom){
+        this(nom,ETAT_BASE);
+    }
+    public Territoire(String nom, Etat etatBase){
+        setEtat(etat);
+        this.nom = nom;
+    }
+
+    public Etat getEtat() {
+        return etat;
+    }
+
+    public void setEtat(Etat etat) {
+        this.etat = etat;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 
 
 }
