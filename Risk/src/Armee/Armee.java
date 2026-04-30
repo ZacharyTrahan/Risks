27512f0bb7caf53e8eb81b2a13c5cbec23f9046d
2541218 <2541218@cegeplimoilou.ca>
2026-04-30 12:43:20 -0400

    Création classe Armee avec ses Constructeurs,get/set/validateur

--- Risk/src/Armee/Armee.java
+++ Risk/src/Armee/Armee.java
@@ -1,4 +1,42 @@
 package Armee;
 
-public class Armee {
+public abstract class Armee {
+
+    protected int force;
+    private final static int FORCE_DEFAUT = 10;
+    private final static int FORCE_MIN = 1;
+
+
+    public Armee(int force) {
+        setForce(force);
+    }
+
+    public Armee() {
+        this(FORCE_DEFAUT);
+    }
+
+    public int getForce() {
+        return force;
+    }
+
+    public void setForce(int force) {
+        if (!isValidForce(force)) {
+            throw new IllegalArgumentException("Force invalide");
+        } else {
+            this.force = force;
+        }
+    }
+
+    public boolean isValidForce(int force) {
+        if (force < FORCE_MIN) {
+            return false;
+
+        } else {
+            return true;
+
+        }
+
+
+    }
+
 }
