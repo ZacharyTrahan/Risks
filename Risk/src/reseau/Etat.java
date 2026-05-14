12717c2c3bcb59b43895e4cc7fe43372f7c106e3
PhilippeSavard <2542496@cegeplimoilou.ca>
2026-05-13 20:59:28 -0400

    Implémentation de Javadoc Etat, Territoire (iA)

--- Risk/src/reseau/Etat.java
+++ Risk/src/reseau/Etat.java
@@ -1,5 +1,9 @@
 package reseau;
-
+/**
+ * Représente l'état d'un territoire dans la partie.
+ *
+ * <p>Chaque valeur correspond à un joueur ou à un territoire neutre.</p>
+ */
 public enum Etat {
     JOUEUR1, JOUEUR2, JOUEUR3, JOUEUR4, JOUEUR5, JOUEUR6, NEUTRE
 
